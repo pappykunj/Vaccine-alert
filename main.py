53fd10fa51332c15d365c238b3755e45b31a6b11
Tech Rick <45835951+richu101@users.noreply.github.com>
2021-05-10 12:59:12 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=31-03-2021'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+result = requests.get(url, headers=headers)
+print(result.json())

7e42fa5f4de46e018a0b07b7eaca81f28878515f
Akshay Joseph <47769124+akshayjoseph666@users.noreply.github.com>
2021-05-10 14:07:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import requests
 import json
-url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=10-05-2021'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-result = requests.get(url, headers=headers)
+import numpy as np
+from datetime import date,datetime,timedelta
+
+day0=datetime.now()
+day1=day0+timedelta(1)
+day2=day0+timedelta(2)
+day3=day0+timedelta(3)
+day4=day0+timedelta(4)
+day5=day0+timedelta(5)
+day6=day0+timedelta(6)
+
+Week=np.array([day0,day1,day2,day3,day4,day5,day6])
+
+	
+#print("date = ", day0.strftime('%d-%m-%Y'))
+
+
+
+
+
 
 #print(result.json())
-new_result=result.json()
-
-num=len(new_result['sessions'])
-num1=num
-
-for i in range(num):
-     print(i+1,new_result['sessions'][i]['center_id'],new_result['sessions'][i]['name'],new_result['sessions'][i]['min_age_limit'],new_result['sessions'][i]['available_capacity'],new_result['sessions'][i]['fee_type'],new_result['sessions'][i]['vaccine'])
 
 
-    
 
-print("Total sessions : ",num1)
 
-#print(new_result['sessions'][0]['center_id'])
-#print(new_result['sessions'][0]['name'])
+def GetData():
+	for x in range(7):
+		Date=Week[x].strftime('%d-%m-%Y')
+		print(Date)
+		url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date={}'.format(Date)
+		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+		result = requests.get(url, headers=headers)
+		new_result=result.json()
+		num=len(new_result['sessions'])
+		num1=num
+		for i in range(num):
+			print(i+1,new_result['sessions'][i]['center_id'],new_result['sessions'][i]['name'],new_result['sessions'][i]['min_age_limit'],new_result['sessions'][i]['available_capacity'],new_result['sessions'][i]['fee_type'],new_result['sessions'][i]['vaccine'])
+		print("Total sessions : ",num1)
+
+GetData()
+

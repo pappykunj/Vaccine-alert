5a395e56b531fdbc1c1c67c4785d3586c1dec14e
Akshay Joseph <47769124+akshayjoseph666@users.noreply.github.com>
2021-05-11 09:13:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,36 +14,43 @@ day6=day0+timedelta(6)
 
 Week=np.array([day0,day1,day2,day3,day4,day5,day6])
 
-	
-#print("date = ", day0.strftime('%d-%m-%Y'))
-
-
-
-
-
-
-#print(result.json())
-
-
-
+dataB1=np.array([])
+data1 = 0
 
 def GetData():
+	global dataB1
+	global data1
+	
 	for x in range(7):
 		Date=Week[x].strftime('%d-%m-%Y')
-		print(Date)
+		
 		url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date={}'.format(Date)
 		headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
 		result = requests.get(url, headers=headers)
 		new_result=result.json()
 		num=len(new_result['sessions'])
+
+		if num>0:
+			print('\n')
+			print("Date : ",Date)
 		num1=num
 		for i in range(num):
 			print(i+1,new_result['sessions'][i]['center_id'],new_result['sessions'][i]['name'],new_result['sessions'][i]['min_age_limit'],new_result['sessions'][i]['available_capacity'],new_result['sessions'][i]['fee_type'],new_result['sessions'][i]['vaccine'])
-		print("Total sessions : ",num1)
+			dataB1=np.insert(dataB1,i,i+1)
 
 
+	NOA=len(dataB1)
+	print("\nDataBase:",dataB1)
+	print("Total Sessions : ",NOA)
+	for i in range(NOA-1,-1,-1):
+		dataB1=np.delete(dataB1,i)
+	print(dataB1)
+	print(len(dataB1))
+	
 
-
+	if NOA!=data1:
+		print("Update available")
+		data1=NOA
 
 def loop1():
 	loop2()
@@ -51,8 +58,12 @@ def loop1():
 def loop2():
 			
 	GetData()
-	time.sleep(32)
+	time.sleep(64)#32
 	loop1()
 
 
 loop1()
+#GetData()
+
+
+
